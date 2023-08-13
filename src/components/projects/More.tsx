6340937c9fa9b897971f698b94947403e164d20f
Yahir Moreno Barajas <100240827+BarajasY@users.noreply.github.com>
2023-08-13 08:20:53 -0600

    UI changes,

--- src/components/projects/More.tsx
+++ src/components/projects/More.tsx
@@ -5,7 +5,7 @@ const More: Component = () => {
   return (
     <div class={style.MoreContainer}>
       <h1>Hungry for <span>more?</span></h1>
-      <p>I have more than <span>20</span> projects already finished.</p>
+      <p>I've only displayed some of my most notable projects, I have more than <span>30</span> repositories finished.</p>
       <p>
         Check them out in my <a href="https://github.com/BarajasY" target="_blank">github</a>!
       </p>
